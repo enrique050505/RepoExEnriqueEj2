c4e4b5e5143c73c4cb13cb15618ec987a51a6424
Alumno18 <Alumno18@DESKTOP-6K48QVF>
2024-05-28 10:15:06 +0200

    subo funcionalidad nueva en la clase AnalisisEdad

--- src/AnalisisEdad.java
+++ src/AnalisisEdad.java
@@ -1,4 +1,31 @@
+import java.util.*;
+
 
 public class AnalisisEdad {
+	
+	private ArrayList<Integer> edades = new ArrayList<Integer>();
 
+	public AnalisisEdad(ArrayList<Integer> edades) {
+		this.edades = edades;
+	}
+	
+	
+	public String devolverFrase(ArrayList<Integer> edades) {
+		String frase = null;
+		int jubilados = 0, mayoresEdad = 0, menoresEdad = 0;
+		
+		for(int i=0; i<edades.size(); i++) {
+			if(edades.get(i)<18) {
+				menoresEdad= menoresEdad + 1;
+			}else if (edades.get(i)>=18 || edades.get(i)<65) {
+				mayoresEdad = mayoresEdad +1;
+			}else {
+				jubilados = jubilados +1;
+			}
+		}
+		
+		return "Menores de Edad " + menoresEdad + ", Mayores de Edad " + mayoresEdad + ", Jubilados " + jubilados;
+
+	}
+	
 }
